29eb4273d56ed65c0a4ca680622385131e05984c
a04k <63ahmedkhaled@gmail.com>
2024-05-02 04:44:01 +0300

    fixed typeError causing build fail

--- src/app/Components/cityTime.tsx
+++ src/app/Components/cityTime.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import { useState, useEffect } from "react";
+
 export default function CityTime() {
-  const [currentTime, setCurrentTime] = useState('00:00');
+  const [currentTime, setCurrentTime] = useState('00:00'); // Initial value as string
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const options = {
+      const timeInCairo = new Date().toLocaleTimeString('en-US', {
         timeZone: 'Africa/Cairo',
-        hour: 'numeric',
-        minute: 'numeric',
-      };
-      const timeInCairo = new Date().toLocaleTimeString('en-US', options);
+        hour: 'numeric', 
+        minute: 'numeric'
+      });
       setCurrentTime(timeInCairo);
-    }, 1000); //making this update every minute doesnt give you a value onload, but also if it did you'll have incorrect times unless it loads on the first second of the new minute
+    }, 1000);
 
-    return () => clearInterval(intervalId); 
-  }, [currentTime]); 
+    return () => clearInterval(intervalId);
+  }, [currentTime]); // Empty dependency array for continuous updates
 
   return (
     <div>
